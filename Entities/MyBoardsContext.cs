855ecbb6efafafa282e95ee5c17a778401b97696
TheLukCraft <luk1997r@o2.pl>
2023-03-22 14:25:55 +0100

    Adding DBContext and MsSql connection configuration

---
+++ Entities/MyBoardsContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyBoards.Entities
+{
+    public class MyBoardsContext : DbContext
+    {
+        public MyBoardsContext(DbContextOptions<MyBoardsContext> options) : base(options)
+        {
+        }
+
+        public DbSet<WorkItem> WorkItems { get; set; }
+        public DbSet<User> users { get; set; }
+        public DbSet<Tag> tags { get; set; }
+        public DbSet<Comment> comments { get; set; }
+        public DbSet<Address> addresses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer();
+        }
+    }
+}
