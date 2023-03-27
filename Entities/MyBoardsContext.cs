ec83ddbd280d2acb8e9b8cca0d81fb752ca996da
TheLukCraft <luk1997r@o2.pl>
2023-03-27 18:46:51 +0200

    Model Seed

--- Entities/MyBoardsContext.cs
+++ Entities/MyBoardsContext.cs
@@ -92,6 +92,11 @@ namespace MyBoards.Entities
                 .WithOne(u => u.User)
                 .HasForeignKey<Address>(a => a.UserId);
             });
+
+            modelBuilder.Entity<WorkItemState>()
+                .HasData(new WorkItemState() { Id = 1, Value = "To Do" },
+                new WorkItemState() { Id = 2, Value = "Doing" },
+                new WorkItemState() { Id = 3, Value = "Done" });
         }
     }
 }
