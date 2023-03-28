7793a761e9a33431735b04b0b0df831125f10310
TheLukCraft <luk1997r@o2.pl>
2023-03-29 00:51:59 +0200

    Manual migration adjustment

---
+++ Migrations/20230328224450_AdditionWorkItemStateSeed.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyBoards.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdditionWorkItemStateSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(table: "WorkItemStates",
+                column: "Value",
+                value: "On Hold");
+
+            migrationBuilder.InsertData(table: "WorkItemStates",
+                  column: "Value",
+               value: "Rejected");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "WorkItemStates",
+                keyColumn: "Value",
+                keyValue: "On Hold");
+
+            migrationBuilder.DeleteData(
+                table: "WorkItemStates",
+                keyColumn: "Value",
+                keyValue: "Rejected");
+        }
+    }
+}
